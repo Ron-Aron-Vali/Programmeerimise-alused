6d5925f7ab15c9cf73e453c958df1f5561d3e05c
Opilane TTHK <opilane@tthk.ee>
2022-11-16 11:21:14 +0200

    if ja else

--- 16.11/ConsoleApp1/ConsoleApp1/Program.cs
+++ 16.11/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,27 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a number!");
+            string number = Console.ReadLine();
+            int numberParsed = Int16.Parse(number);
+
+            if (numberParsed < 1)
+            {
+                Console.WriteLine("number");
+            }
+            else if (numberParsed >= 1 && numberParsed <= 50)
+            {
+                Console.WriteLine("Suurem kui 1, väiksem kui 50");
+            }
+            else if (numberParsed > 50 && numberParsed < 99)
+            {
+                Console.WriteLine("Suurem kui 50, väiksem kui 99");
+            }
+            else
+            { 
+                Console.WriteLine("ERROR");
+            }
+
         }
     }
 }
